6492f13019ec4b2c099123066fbad7cbb185bde7
prasad <prasadmetkar333@gmail.com>
2024-09-13 14:35:13 +0530

    database updated

--- src/Config/db.js
+++ src/Config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
+const env = require('dotenv')
+env.config()
 
-mongoose.connect(`mongodb://0.0.0.0/jobNexus`)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.g02l0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
 .then(()=>{
     console.log("Database connected")
 
-}).catch(()=>{
-    console.log("database not connected")
+}).catch((err)=>{
+    console.log("database not connected", err)
 })
 
 
