0c1992fa2b19fd713ef76fb471599d80e81e771c
prasad <prasadmetkar333@gmail.com>
2024-09-13 15:55:34 +0530

    all data added in dataset

--- src/Config/db.js
+++ src/Config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 const env = require('dotenv')
-env.config()
+env.config()   
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.g02l0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+
+
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.g02l0.mongodb.net/jobNexus?retryWrites=true&w=majority`)
 .then(()=>{
     console.log("Database connected")
 
